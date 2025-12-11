bece16750139574848549feb88e81fbccfe48f60
Laurent Boyer <boyerlaurent30@gmail.com>
2025-12-11 14:57:03 +0100

    feat: Ajouter un workflow de déploiement sur GitHub Pages et configurer Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://laurentboyer1976.github.io',
+  base: '/TutoHTMLMedia',
   srcDir: './src',
   publicDir: './public',
   outDir: './dist',
